c1e1ba61e84667de4046db7c76416dbbca15501a
Harshit Gajjar <hrstgajjar3@gmail.com>
2020-06-24 15:59:07 +0400

    polyline added to map

--- Controller/BeganRunVC.swift
+++ Controller/BeganRunVC.swift
@@ -24,33 +24,55 @@ class BeganRunVC: LocationVC {
     override func viewDidLoad() {
         super.viewDidLoad()
         checkLocationAuthStatus()
-        mapView.delegate = self
     }
     
     override func viewWillAppear(_ animated: Bool) {
         manager?.delegate = self
+        mapView.delegate = self
         manager?.startUpdatingLocation()
-        getLastRun()
+      //  getLastRun()
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        setupMapView()
     }
     
     override func viewDidDisappear(_ animated: Bool) {
         manager?.stopUpdatingLocation()
     }
     
-    func getLastRun(){
-        guard let run =  Run.getAllRuns()?.first else {
+    func setupMapView(){
+        if let overlay = addLastRunToMap(){
+            if mapView.overlays.count > 0{
+                // map already has overlay, so we need to remove that first
+                mapView.removeOverlays(mapView.overlays)
+            }
+            mapView.addOverlay(overlay)
+            lastRunView.isHidden = false
+            lastrunStack.isHidden = false
+            closeBtn.isHidden = false
+        }else{
             lastRunView.isHidden = true
             lastrunStack.isHidden = true
             closeBtn.isHidden = true
-            return
         }
-        lastRunView.isHidden = false
-        lastrunStack.isHidden = false
-        closeBtn.isHidden = false
+    }
+    
+    func addLastRunToMap() -> MKPolyline?{
+        guard let run =  Run.getAllRuns()?.first else { return nil }
         paceLbl.text = run.pace.formatTimeDurationToString()
         durationLbl.text = run.duration.formatTimeDurationToString()
         distancelbl.text = "\(run.distance.meterToMiles(places: 2)) mi"
+        
+        var coordinate = [CLLocationCoordinate2D]()
+        
+        for location in run.locations{
+            coordinate.append(CLLocationCoordinate2D(latitude: location.latitude, longitude: location.longitude))
+        }
+        
+        return MKPolyline(coordinates: coordinate, count: run.locations.count)
     }
+    
     
     @IBAction func locationCentrebtnPressed(_ sender: Any) {
     }
@@ -70,5 +92,15 @@ extension BeganRunVC: CLLocationManagerDelegate{
             mapView.showsUserLocation = true
             mapView.userTrackingMode = .follow // to get that blue circle which keeps moving, depending upon our location
         }
+    }
+    
+    // to draw polyline
+    func mapView(_ mapView: MKMapView, rendererFor overlay: MKOverlay) -> MKOverlayRenderer {
+        
+        let polyline = overlay as! MKPolyline
+        let renderer = MKPolylineRenderer(polyline: polyline)
+        renderer.strokeColor = #colorLiteral(red: 0.2588235438, green: 0.7568627596, blue: 0.9686274529, alpha: 1)
+        renderer.lineWidth = 4
+        return renderer
     }
 }
